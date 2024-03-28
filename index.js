4e6b2f099c45ad863e9b79d2a145d8c521243330
faiyaz032 <faiyazrahman03@gmail.com>
2024-03-28 22:49:53 +0600

    Hello there

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 8001;
 app.get('/', (req, res) => {
   res.json({
     success: true,
-    message: 'Hello, You deployed your server to ec2 successfully. Is it working?',
+    message:
+      'Hello, You deployed your server to ec2 successfully. Is it working?  Abbas bhai Zindabad',
   });
 });
 
